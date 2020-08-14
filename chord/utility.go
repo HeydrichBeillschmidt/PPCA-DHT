c08d8db97a463eab86a220fb4ebf6393697b97a4
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2020-08-14 23:28:51 +0800

    expanded function packs

--- chord/utility.go
+++ chord/utility.go
@@ -21,7 +21,18 @@ func Range_Check(val, beg, end *big.Int, RightClosure bool) bool {
 	}
 }
 
-func Get_Local_Address() (localaddress string) {
+func Power_Two(id *big.Int, exponent uint) *big.Int {
+	ans := big.NewInt(1)
+	ans.Lsh(ans, exponent)
+	ans.Add(ans, id)
+	if ans.BitLen() > RING_SIZE {
+		tmp := big.NewInt(1)
+		ans.Mod(ans, tmp.Lsh(tmp, uint(RING_SIZE)))
+	}
+	return ans
+}
+
+func GetLocalAddress() (localaddress string) {
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		panic("init: failed to find network interfaces")
@@ -53,20 +64,18 @@ func Get_Local_Address() (localaddress string) {
 	return localaddress
 }
 
-func Close(client *rpc.Client) bool {
+func Close(client *rpc.Client) error {
 	err := client.Close()
 	if err != nil {
 		fmt.Println("Client Closure Failed: ", err)
-		return true
 	}
-	return false
+	return err
 }
 
-func Dial(addr string) (*rpc.Client, bool) {
+func Dial(addr string) (*rpc.Client, error) {
 	client, err := rpc.Dial("tcp", addr)
 	if err != nil {
 		fmt.Println("Dialing Failed: ", err)
-		return client, true
 	}
-	return client, false
+	return client, err
 }
