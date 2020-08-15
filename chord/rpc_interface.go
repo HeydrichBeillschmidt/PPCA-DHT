8a9a8193f898b21219792a23ca59b09c10cbf996
Heydrich_B <56252029+HeydrichBeillschmidt@users.noreply.github.com>
2020-08-15 10:32:10 +0800

    formalized

--- chord/rpc_interface.go
+++ chord/rpc_interface.go
@@ -16,7 +16,7 @@ type RPCNode struct {
 	Port 			  int
 }
 
-func (node *RPCNode) Initialize(port int) {
+func Initialize(node *RPCNode, port int) {
 	node.Node = new(LinkNode)
 	node.Node.Initialize(port)
 
@@ -35,70 +35,62 @@ func (node *RPCNode) Initialize(port int) {
 	node.Listener = lis
 }
 
-func (node *RPCNode) Find_Successor(id *big.Int, ans *address_type) error {
+func (node *RPCNode) Find_Successor(id *big.Int, ans *Address_Type) error {
 	return node.Node.Find_Successor(id, ans)
 }
 
-func (node *RPCNode) Get_Predecessor(args int, ans *address_type) error {
+func (node *RPCNode) Get_Predecessor(args int, ans *Address_Type) error {
 	return node.Node.Get_Predecessor(args, ans)
 }
-
-func (node *RPCNode) Get_Successor_List(args int, ans *[SUCCESSOR_LIST_SIZE]address_type) error {
+func (node *RPCNode) Get_Successor_List(args int, ans *[SUCCESSOR_LIST_SIZE]Address_Type) error {
 	return node.Node.Get_Successor_List(args, ans)
 }
 
-func (node *RPCNode) Put_Value(p pair_type, ret *int) error {
-	return node.Node.Put_Value(p, ret)
+func (node *RPCNode) Update_Successor(nw_scsr Address_Type, ret *int) error {
+	return node.Node.Update_Successor(nw_scsr, ret)
+}
+func (node *RPCNode) Update_Predecessor(nw_prdcsr Address_Type, ret *int) error {
+	return node.Node.Update_Predecessor(nw_prdcsr, ret)
 }
 
-func (node *RPCNode) Put_Value_Backup(p pair_type, ret *int) error {
-	return node.Node.Put_Value_Backup(p, ret)
-}
-
-func (node *RPCNode) Put_Value_Successor(p pair_type, ret *int) error {
-	return node.Node.Put_Value_Successor(p, ret)
+func (node *RPCNode) Notify(pred Address_Type, ret *int) error {
+	return node.Node.Notify(pred, ret)
 }
 
 func (node *RPCNode) Get_Value(key string, ans *string) error {
 	return node.Node.Get_Value(key, ans)
 }
 
+func (node *RPCNode) Put_Value(p Pair_Type, ret *int) error {
+	return node.Node.Put_Value(p, ret)
+}
+func (node *RPCNode) Put_Value_Backup(p Pair_Type, ret *int) error {
+	return node.Node.Put_Value_Backup(p, ret)
+}
+func (node *RPCNode) Put_Value_Successor_Backup(p Pair_Type, ret *int) error {
+	return node.Node.Put_Value_Successor_Backup(p, ret)
+}
+
 func (node *RPCNode) Delete_Key(key string, successful *bool) error {
 	return node.Node.Delete_Key(key, successful)
 }
-
 func (node *RPCNode) Delete_Key_Backup(key string, successful *bool) error {
 	return node.Node.Delete_Key_Backup(key, successful)
 }
-
-func (node *RPCNode) Delete_Key_Successor(key string, successful *bool) error {
-	return node.Node.Delete_Key_Successor(key, successful)
+func (node *RPCNode) Delete_Key_Successor_Backup(key string, successful *bool) error {
+	return node.Node.Delete_Key_Successor_Backup(key, successful)
 }
 
-func (node *RPCNode) Update_Successor(nw_scsr address_type, ret *int) error {
-	return node.Node.Update_Successor(nw_scsr, ret)
+func (node *RPCNode) Deliver_Part(node_joined *big.Int, ans *map[string]string) error {
+	return node.Node.Deliver_Part(node_joined, ans)
 }
-
-func (node *RPCNode) Update_Predecessor(nw_prdcsr address_type, ret *int) error {
-	return node.Node.Update_Predecessor(nw_prdcsr, ret)
-}
-
-func (node *RPCNode) Deliver(node_joined *big.Int, ans *map[string]string) error {
-	return node.Node.Deliver(node_joined, ans)
-}
-
 func (node *RPCNode) Deliver_Backup(args int, ans *map[string]string) error {
 	return node.Node.Deliver_Backup(args, ans)
 }
 
-func (node *RPCNode) Deliver_Quit(delivery *data_type, ret *int) error {
-	return node.Node.Deliver_Quit(delivery, ret)
+func (node *RPCNode) Receive_Quit(delivery *Data_Type, ret *int) error {
+	return node.Node.Receive_Quit(delivery, ret)
 }
-
-func (node *RPCNode) Deliver_Quit_Backup(delivery *data_type, ret *int) error {
-	return node.Node.Deliver_Quit_Backup(delivery, ret)
-}
-
-func (node *RPCNode) Notify(pred *address_type, ret *int) error {
-	return node.Node.Notify(pred, ret)
+func (node *RPCNode) Receive_Quit_Backup(delivery *Data_Type, ret *int) error {
+	return node.Node.Receive_Quit_Backup(delivery, ret)
 }
